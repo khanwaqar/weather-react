c0cff1bd73f2dd69f96eaed36c78c6540ec8d627
Waqar <khan.waqar278@gmail.com>
2023-02-02 14:35:32 +0500

    added rain css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 const api = {
   key: "c6f0a0bada9354531b46c50f2f7a02bc",
   base: "https://api.openweathermap.org/data/2.5/"
@@ -32,9 +33,30 @@ function App() {
     return `${day} ${date} ${month} ${year}`
   }
 
+  function convertEpoch(value, timeoffset) {
+
+    console.log(timeoffset);
+    console.log(value)
+    if (!value) {
+      return ''
+    }
+
+    let dateObj = new Date(parseInt(value+timeoffset) * 1000);
+    let hours = dateObj.getUTCHours();
+    let minutes = dateObj.getUTCMinutes();
+    let seconds = dateObj.getSeconds();
+
+    let timeString = hours.toString().padStart(2, '0') + ':' + 
+    minutes.toString().padStart(2, '0') + ':' + 
+    seconds.toString().padStart(2, '0');
+
+    return timeString
+  
+    }
+
   return (
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
-      <main>
+      <main className={(typeof weather.rain != "undefined") ? 'weather rain' : "weather"}>
         <div className="search-box">
           <input 
             type="text"
@@ -55,8 +77,27 @@ function App() {
             <div className="temp">
               {Math.round(weather.main.temp)}°c
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
+            <div className="weather">
+              <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+              {weather.weather[0].main}</div>
           </div>
+
+          <div className="weather-info">
+            <div className="feelsLike"> Feels Like <span>
+              {Math.round(weather.main.feels_like)}°c
+              </span>
+            </div>
+            <div className="humidity">Humidity <span>{weather.main.humidity}</span></div>
+            <div className="wind">Wind Speed <span>{weather.wind.speed}</span> at <span>{weather.wind.deg}°</span></div>
+          </div>
+
+          <div className="sun-timings">
+            <div className="sunrise"> Sunrise: 
+              {convertEpoch(weather.sys.sunrise ,weather.timezone)}
+            </div>
+            <div className="sunset">Sunset: {convertEpoch(weather.sys.sunset,weather.timezone)}</div>
+          </div>
+
         </div>
         ) : ('')}
       </main>
